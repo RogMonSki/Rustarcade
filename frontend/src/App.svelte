8d756d01f9559965375398bbc1bb38acc3721f9f
RogMonSki <szymek108@gmail.com>
2026-04-10 00:22:09 +0100

    Added some basic code

---
+++ frontend/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import init, { greet } from 'game-core';
+  import wasmUrl from 'game-core/game_core_bg.wasm?url';
+
+  let message = '';
+
+  async function loadWasm() {
+    try {
+      await init(wasmUrl);
+      message = greet();
+    } catch (e) {
+      message = 'Error: ' + e;
+      console.error(e);
+    }
+  }
+
+  loadWasm()
+</script>
+
+<main class="flex items-center justify-center h-screen bg-gray-900">
+  <p class="text-white text-2xl">{message}</p>
+</main>
---
+++ frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
+import wasm from 'vite-plugin-wasm'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    wasm(),
+  ],
+})
